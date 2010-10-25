14db2afa1e7882a6a176e4f4ab32b0bcd11c6cd0
Petr Gladkikh <PetrGlad@gmail.com>
2010-10-25 16:58:10 +0700

    Minor cleanup (trying to restore sources).

--- src/petrglad/dependencytree/Activator.java
+++ src/petrglad/dependencytree/Activator.java
@@ -5,14 +5,18 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
- * The activator class controls the plug-in life cycle
+ * The activator class controls the plugin life cycle.
  */
 public class Activator extends AbstractUIPlugin {
 
-    // The plug-in ID
+    /**
+     * The plug-in ID
+     */
     public static final String PLUGIN_ID = "petrglad.dependencytree";
 
-    // The shared instance
+    /**
+     * The shared instance
+     */
     private static Activator plugin;
 
     public Activator() {
